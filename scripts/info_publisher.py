efca20efc11f70397456f1f90afd29c934b00b0e
witalop <witalopietler@gmail.com>
2019-05-29 17:04:52 -0300

    Att info msg

--- scripts/info_publisher.py
+++ scripts/info_publisher.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python
 import rospy
+import json
 from virosquad.msg import info
 
 def info_publisher():
     topic = rospy.Publisher('info', info, queue_size=1)
     rospy.init_node('info_publisher', anonymous=True)
-    rate = rospy.Rate(10)
+    rate = rospy.Rate(1)
     
     msg = info()
     while not rospy.is_shutdown():
-            msg.vehicle = 'Fusca'
-            msg.info = 'Veiculo do Hitler'
-            msg.description = 'Veiculo extremamente potente criado no calor da guerra'
+        with open('/home/witalo/catkin_ws/src/virosquad/scripts/info.json') as json_file:
+            data = json.load(json_file)
 
-            topic.publish(msg)
+        msg.header.stamp = rospy.get_rostime()
+        msg.id = data['id']
+        msg.vehicle = data['vehicle']
+        msg.info = data['info']
+        msg.details = data['details']
+        topic.publish(msg)
             
 if __name__ == '__main__':
-        try:
-            info_publisher()
-        except rospy.ROSInterruptException:
-            pass
+    try:
+        info_publisher()
+    except rospy.ROSInterruptException:
+        pass
